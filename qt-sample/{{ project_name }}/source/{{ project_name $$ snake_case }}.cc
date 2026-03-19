91525b9bcd7c3bb874652a56f4ea53f0183076b0
Shamaoke <shamaoke@yandex.ru>
2026-03-19 16:10:53 +0300

    [code] Develop `qt-sample`

--- qt-sample/{{ project_name }}/source/{{ project_name $$ snake_case }}.cc
+++ qt-sample/{{ project_name }}/source/{{ project_name $$ snake_case }}.cc
@@ -1,7 +1,13 @@
 
+#include <iostream>
+
 #include "include/{{ project_name | snake_case }}.hh"
 
-{{ project_name | upper_camel_case }}::{{ project_name | upper_camel_case }}(QObject* parent) : QObject { parent } { }
+{{ project_name | upper_camel_case }}::{{ project_name | upper_camel_case }}(QObject* parent) :
+  QObject { parent }
+{
+  std::cout << "Welcome to {{ project_group_name }} | {{ project_name | replace(from="-", to=" ") | capitalize }}!" << "\n";
+}
 
 {{ project_name | upper_camel_case }}::~{{ project_name | upper_camel_case }}( ) { }
 
